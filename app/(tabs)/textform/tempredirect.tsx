7100d986c56d880579fba6024969169fee6a01b2
zeithrold <zeithrold@gmail.com>
2023-09-08 18:06:48 +0800

    fixed index config load error

--- app/(tabs)/textform/tempredirect.tsx
+++ app/(tabs)/textform/tempredirect.tsx
@@ -1,6 +1,11 @@
-import { Redirect } from 'expo-router'
+import { Redirect, useFocusEffect } from 'expo-router'
+import { useConfigMutation } from 'lib/hooks'
 import React from 'react'
 
 export default function TempRedirect () {
+  const { trigger } = useConfigMutation()
+  useFocusEffect(React.useCallback(() => {
+    void trigger()
+  }, []))
   return <Redirect href='/(tabs)' />
 }
